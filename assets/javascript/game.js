334ff144498d5e61f44ce91aee7040eab3159e0e
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-02-17 15:01:36 -0500

    basic html functioning except displaying letter guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,33 @@
-// Create array of possible word choices
+//Set up buttons
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+for (var i = 0; i < alphabet.length; i++) {
+
+    var $letterBtn = $("<button>")
+        .addClass("letter-button")
+        .attr("data-letter", alphabet[i])
+        .html(alphabet[i]);
+
+    $("#letter-options").append($letterBtn);
+}
+
+// Create array of possible word choices 
 var wordChoices = ["CAT", "DOG", "MOUSE"];
 var word = "";
 var wordArray = [];
 var userWord = "";
 var userWordArray = [];
-// Create array for letters guessed -> will want to display this but only display the letters not in the word array
+// Create array for letters guessed (incorrectly)
 var lettersGuessed = [];
 // Start counting guesses remaining
-var guessesRemaining = 3;
+var guessesRemaining
 // Tracking Wins/Loses
 var win = 0;
 var lose = 0;
 
-// Reset for next game
-function reset () {
-    word = "";
-    wordArray = [];
-    userWord = "";
-    userWordArray = [];
-    lettersGuessed = [];
-    guessesRemaining = 3;
-}
-// Pick a random word from the array and assign it as the one trying to be guessed
+// Rest game and pick a random word from the array and assign it as the one trying to be guessed
 function newWord() {
-    reset ();
+    reset();
     word = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
     // //Turn this word into an array of letters
@@ -34,12 +38,16 @@ function newWord() {
         userWordArray.push(" _ ");
     }
     $("#userWord").html(userWordArray);
-    //Display guesses remaining
-    console.log("guessesRemaining" + guessesRemaining);
-    //Display letters guessed
-    console.log("letterGuessed:" + lettersGuessed)
 }
-
+// Reset for next game
+function reset() {
+    word = "";
+    wordArray = [];
+    userWord = "";
+    userWordArray = [];
+    lettersGuessed = [];
+    guessesRemaining = 6;
+}
 // Check to see if letter guessed has been guessed before
 function checkGuessedBefore(userGuess) {
     guessed = false;
@@ -50,7 +58,6 @@ function checkGuessedBefore(userGuess) {
             return;
         }
     }
-
     for (i = 0; i < userWordArray.length; i++) {
         if (userGuess === userWordArray[i]) {
             alert("Letter " + userWordArray[i] + " has already been guessed correctly.");
@@ -68,19 +75,106 @@ function checkGuess(userGuess) {
         if (userGuess === wordArray[i]) {
             userWordArray.splice(i, 1, userGuess);
             $("#userWord").html(userWordArray);
-            console.log(userWordArray);
             gotOne = true;
         }
     }
-    // If wrong, -1 for guesses remaining and add the letter to letters guessed
+    // If wrong, -1 for guesses remaining, add the letter to letters guessed, and then draw part of hangman
     if (gotOne === false) {
         guessesRemaining -= 1;
-        console.log(guessesRemaining);
+        console.log("guessesRemaining " + guessesRemaining);
         lettersGuessed.push(userGuess);
-        console.log(lettersGuessed);
+        $("#letters-guessed").append(userGuess);
+        drawHangman(guessesRemaining);
     }
 }
-// }
+//Drawing the hangman
+function drawHangman(guessesRemaining) {
+    var $canvas = $(canvas);
+
+    if (guessesRemaining === 5) {
+        //left leg
+        $canvas.drawLine({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            rounded: true,
+            x1: 25, y1: 25,
+            x2: 50, y2: 75,
+        });
+    }
+    if (guessesRemaining === 4) {
+        //right leg
+        $canvas.drawLine({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            rounded: true,
+            x1: 75, y1: 25,
+            x2: 50, y2: 75,
+        });
+    }
+    if (guessesRemaining === 3) {
+        //body
+        $canvas.drawLine({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            rounded: true,
+            x1: 50, y1: 75,
+            x2: 50, y2: 150,
+        });
+    }
+    if (guessesRemaining === 2) {
+        //left arm
+        $canvas.drawLine({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            rounded: true,
+            x1: 50, y1: 125,
+            x2: 25, y2: 150,
+        });
+    }
+    if (guessesRemaining === 1) {
+        //right arm
+        $canvas.drawLine({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            rounded: true,
+            x1: 50, y1: 125,
+            x2: 75, y2: 150,
+        });
+    }
+    if (guessesRemaining === 0) {
+        //head
+        $canvas.drawArc({
+            strokeStyle: "black",
+            strokeWidth: 4,
+            x: 50, y: 175,
+            radius: 25,
+        })
+            //left eye
+            .drawArc({
+                fillStyle: "black",
+                strokeStyle: "black",
+                x: 40, y: 180,
+                radius: 2,
+            })
+            //right eye
+            .drawArc({
+                fillStyle: "black",
+                strokeStyle: "black",
+                x: 60, y: 180,
+                radius: 2,
+            })
+            //mouth
+            .drawArc({
+                strokeStyle: "black",
+                strokeWidth: 2,
+                x: 50, y: 165,
+                radius: 5,
+            });
+    }
+    else if (guessesRemaining === 5) {
+
+    }
+}
 
 //check to see if user has won or lost and track win/loss
 function checkStatus() {
@@ -90,21 +184,21 @@ function checkStatus() {
         setTimeout(function () {
             alert("You win, Liberacorpus!");
             win += 1;
-            console.log("wins:" + win);
+            $("#wins").html("Wins: " + win);
             newWord();
         }, 50);
     }
     else if (guessesRemaining === 0) {
         alert("You lose, Levicorpus! The word was " + word + ".");
-        lose -= 1;
-        console.log("loses:" + lose);
+        lose += 1;
+        $("#losses").html("Losses: " + lose);
         newWord()
     }
 }
 
 
-$(document).ready(function() {
-newWord();
+$(document).ready(function () {
+    newWord();
 });
 
 //User will press a key to guess a letter
